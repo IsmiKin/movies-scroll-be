cb4dd39b7f0657094611999a940b43391d633ede
IsmiKin <ismikin@gmail.com>
2018-11-12 21:25:53 +0100

    Add firebase, debugger and autoreload
    - Firebase admin
    
    - Nodemon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 const express = require("express")
 const app = express()
+const admin = require('firebase-admin')
+
+const firebaseAccount = require('../movies-scroll-04b543d840f6.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseAccount)
+})
+
+const db = admin.firestore()
+
 
 app.get("/", (req, res) => {
   return res.status(200).send('pika')
+})
+
+app.get("/scrolls", (req, res) => {
+  db.collection('scrolls').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data())
+      })
+      return res
+        .status(200)
+        .json({scrolls: snapshot.docs.map(doc => doc.data()) })
+    })
+    .catch((err) => {
+      return res.status(500).send(`Error getting documents ${err}`)
+    })
 })
 
 const server = app.listen(5000, () =>
