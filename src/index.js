09cc44fe1aea0673e41f0317fcd37da7381c6f8c
Ismael Ors <ismikin@gmail.com>
2018-11-13 10:34:40 +0100

    Firebase config file with env variables

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 const express = require("express")
+const firebaseAdmin = require('firebase-admin')
+
+let firebaseAccount = require('../firebase-key.json')
+
+firebaseAccount = {
+  ...firebaseAccount,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  client_x509_cert_url: process.env.FIREBASE_CERT_URL
+}
+
 const app = express()
-const admin = require('firebase-admin')
 
-const firebaseAccount = require('../movies-scroll-04b543d840f6.json')
-
-admin.initializeApp({
-  credential: admin.credential.cert(firebaseAccount)
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(firebaseAccount)
 })
 
-const db = admin.firestore()
-
+const db = firebaseAdmin.firestore()
 
 app.get("/", (req, res) => {
   return res.status(200).send('pika')
