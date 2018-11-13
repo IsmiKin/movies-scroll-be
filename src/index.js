87ed78f1472158f90feb1521ef71306ec9fa6ee0
Ismael Ors <ismikin@gmail.com>
2018-11-13 11:18:33 +0100

    Decode firebase private key
    Zeit now has limitations for secrets multilines, so it's require to use base64

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ let firebaseAccount = require('../firebase-key.json')
 firebaseAccount = {
   ...firebaseAccount,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('ascii'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   client_x509_cert_url: process.env.FIREBASE_CERT_URL
